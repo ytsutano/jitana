a82b2e367011481310faaecad1f5c0694789d8e9
Yutaka Tsutano <yutaka@tsutano.com>
2015-11-15 00:15:09 -0600

    Put insn_base in detail namespace

--- include/jitana/vm_core/insn.hpp
+++ include/jitana/vm_core/insn.hpp
@@ -17,6 +17,89 @@
 #include <boost/mpl/vector/vector40.hpp>
 
 namespace jitana {
+    namespace detail {
+        template <size_t RegSize, typename ConstVal>
+        struct insn_base {
+            using reg_array_type = std::array<register_idx, RegSize>;
+
+            opcode op;
+            reg_array_type regs;
+            ConstVal const_val;
+
+            insn_base()
+            {
+            }
+
+            insn_base(opcode op, reg_array_type regs, const ConstVal& const_val)
+                    : op(op), regs(regs), const_val(const_val)
+            {
+            }
+
+            bool is_regs_range() const
+            {
+                return RegSize >= 3 && !regs[1].valid() && regs.front().valid()
+                        && regs.back().valid() && regs.front() <= regs.back();
+            }
+
+            friend std::vector<register_idx> defs(const insn_base& x)
+            {
+                const auto& insn_info = info(x.op);
+                std::vector<register_idx> result;
+
+                if (RegSize > 0 && insn_info.sets_register()
+                    && !insn_info.sets_result()) {
+                    result.push_back(x.regs[0]);
+                }
+                if (insn_info.sets_result()) {
+                    result.push_back(register_idx::idx_result);
+                }
+                unique_sort(result);
+                return result;
+            }
+
+            friend std::vector<register_idx> uses(const insn_base& x)
+            {
+                std::vector<register_idx> result;
+                const auto& regs = x.regs;
+                if (x.is_regs_range()) {
+                    result.resize(
+                            static_cast<int32_t>(regs.back() - regs.front()));
+                    std::iota(begin(result), end(result), regs.front());
+                }
+                if (RegSize > 0) {
+                    auto start = begin(regs);
+                    const auto& insn_info = info(x.op);
+                    if (insn_info.sets_register() && !insn_info.sets_result()
+                        && !insn_info.sets_register_inplace()) {
+                        ++start;
+                    }
+                    result.reserve(RegSize);
+                    std::copy(start, end(regs), std::back_inserter(result));
+                    unique_sort(result);
+                }
+                return result;
+            }
+
+            friend std::ostream& operator<<(std::ostream& os,
+                                            const insn_base& x)
+            {
+                os << info(x.op).mnemonic();
+                if (x.is_regs_range()) {
+                    os << " " << x.regs.front() << "-" << x.regs.back();
+                }
+                else {
+                    for (const auto& r : x.regs) {
+                        if (r.valid()) {
+                            os << " " << r;
+                        }
+                    }
+                }
+                os << " [" << x.const_val << "]";
+                return os;
+            }
+        };
+    }
+
     struct no_const_val {
         friend std::ostream& operator<<(std::ostream& os, const no_const_val& x)
         {
@@ -36,125 +119,46 @@ namespace jitana {
         }
     };
 
-    template <size_t RegSize, typename ConstVal>
-    struct insn_base {
-        using reg_array_type = std::array<register_idx, RegSize>;
-
-        opcode op;
-        reg_array_type regs;
-        ConstVal const_val;
-
-        insn_base()
-        {
-        }
-
-        insn_base(opcode op, reg_array_type regs, const ConstVal& const_val)
-                : op(op), regs(regs), const_val(const_val)
-        {
-        }
-
-        bool is_regs_range() const
-        {
-            return RegSize >= 3 && !regs[1].valid() && regs.front().valid()
-                    && regs.back().valid() && regs.front() <= regs.back();
-        }
-
-        friend std::vector<register_idx> defs(const insn_base& x)
-        {
-            const auto& insn_info = info(x.op);
-            std::vector<register_idx> result;
-
-            if (RegSize > 0 && insn_info.sets_register()
-                && !insn_info.sets_result()) {
-                result.push_back(x.regs[0]);
-            }
-            if (insn_info.sets_result()) {
-                result.push_back(register_idx::idx_result);
-            }
-            unique_sort(result);
-            return result;
-        }
-
-        friend std::vector<register_idx> uses(const insn_base& x)
-        {
-            std::vector<register_idx> result;
-            const auto& regs = x.regs;
-            if (x.is_regs_range()) {
-                result.resize(static_cast<int32_t>(regs.back() - regs.front()));
-                std::iota(begin(result), end(result), regs.front());
-            }
-            if (RegSize > 0) {
-                auto start = begin(regs);
-                const auto& insn_info = info(x.op);
-                if (insn_info.sets_register() && !insn_info.sets_result()
-                    && !insn_info.sets_register_inplace()) {
-                    ++start;
-                }
-                result.reserve(RegSize);
-                std::copy(start, end(regs), std::back_inserter(result));
-                unique_sort(result);
-            }
-            return result;
-        }
-
-        friend std::ostream& operator<<(std::ostream& os, const insn_base& x)
-        {
-            os << info(x.op).mnemonic();
-            if (x.is_regs_range()) {
-                os << " " << x.regs.front() << "-" << x.regs.back();
-            }
-            else {
-                for (const auto& r : x.regs) {
-                    if (r.valid()) {
-                        os << " " << r;
-                    }
-                }
-            }
-            os << " [" << x.const_val << "]";
-            return os;
-        }
-    };
-
     // clang-format off
-    struct insn_nop              : insn_base<0, no_const_val  > { using insn_base::insn_base; };
-    struct insn_move             : insn_base<2, no_const_val  > { using insn_base::insn_base; };
-    struct insn_return           : insn_base<1, no_const_val  > { using insn_base::insn_base; };
-    struct insn_const            : insn_base<1, int32_t       > { using insn_base::insn_base; };
-    struct insn_const_wide       : insn_base<1, int64_t       > { using insn_base::insn_base; };
-    struct insn_const_string     : insn_base<1, std::string   > { using insn_base::insn_base; };
-    struct insn_const_class      : insn_base<1, dex_type_hdl  > { using insn_base::insn_base; };
-    struct insn_monitor_enter    : insn_base<1, no_const_val  > { using insn_base::insn_base; };
-    struct insn_monitor_exit     : insn_base<1, no_const_val  > { using insn_base::insn_base; };
-    struct insn_check_cast       : insn_base<1, dex_type_hdl  > { using insn_base::insn_base; };
-    struct insn_instance_of      : insn_base<2, dex_type_hdl  > { using insn_base::insn_base; };
-    struct insn_array_length     : insn_base<2, no_const_val  > { using insn_base::insn_base; };
-    struct insn_new_instance     : insn_base<1, dex_type_hdl  > { using insn_base::insn_base; };
-    struct insn_new_array        : insn_base<2, dex_type_hdl  > { using insn_base::insn_base; };
-    struct insn_filled_new_array : insn_base<5, dex_type_hdl  > { using insn_base::insn_base; };
-    struct insn_fill_array_data  : insn_base<1, array_payload > { using insn_base::insn_base; };
-    struct insn_throw            : insn_base<1, no_const_val  > { using insn_base::insn_base; };
-    struct insn_goto             : insn_base<0, int32_t       > { using insn_base::insn_base; };
-    struct insn_switch           : insn_base<1, no_const_val  > { using insn_base::insn_base; };
-    struct insn_cmp              : insn_base<3, no_const_val  > { using insn_base::insn_base; };
-    struct insn_if               : insn_base<2, no_const_val  > { using insn_base::insn_base; };
-    struct insn_if_z             : insn_base<1, no_const_val  > { using insn_base::insn_base; };
-    struct insn_aget             : insn_base<3, no_const_val  > { using insn_base::insn_base; };
-    struct insn_aput             : insn_base<3, no_const_val  > { using insn_base::insn_base; };
-    struct insn_iget             : insn_base<2, dex_field_hdl > { using insn_base::insn_base; };
-    struct insn_iget_quick       : insn_base<2, uint16_t      > { using insn_base::insn_base; };
-    struct insn_iput             : insn_base<2, dex_field_hdl > { using insn_base::insn_base; };
-    struct insn_iput_quick       : insn_base<2, uint16_t      > { using insn_base::insn_base; };
-    struct insn_sget             : insn_base<1, dex_field_hdl > { using insn_base::insn_base; };
-    struct insn_sput             : insn_base<1, dex_field_hdl > { using insn_base::insn_base; };
-    struct insn_invoke           : insn_base<5, dex_method_hdl> { using insn_base::insn_base; };
-    struct insn_invoke_quick     : insn_base<5, uint16_t      > { using insn_base::insn_base; };
-    struct insn_unary_arith_op   : insn_base<2, no_const_val  > { using insn_base::insn_base; };
-    struct insn_binary_arith_op  : insn_base<3, no_const_val  > { using insn_base::insn_base; };
-    struct insn_aug_assign_op    : insn_base<2, no_const_val  > { using insn_base::insn_base; };
-    struct insn_const_arith_op   : insn_base<2, int16_t       > { using insn_base::insn_base; };
+    struct insn_nop              : detail::insn_base<0, no_const_val  > { using insn_base::insn_base; };
+    struct insn_move             : detail::insn_base<2, no_const_val  > { using insn_base::insn_base; };
+    struct insn_return           : detail::insn_base<1, no_const_val  > { using insn_base::insn_base; };
+    struct insn_const            : detail::insn_base<1, int32_t       > { using insn_base::insn_base; };
+    struct insn_const_wide       : detail::insn_base<1, int64_t       > { using insn_base::insn_base; };
+    struct insn_const_string     : detail::insn_base<1, std::string   > { using insn_base::insn_base; };
+    struct insn_const_class      : detail::insn_base<1, dex_type_hdl  > { using insn_base::insn_base; };
+    struct insn_monitor_enter    : detail::insn_base<1, no_const_val  > { using insn_base::insn_base; };
+    struct insn_monitor_exit     : detail::insn_base<1, no_const_val  > { using insn_base::insn_base; };
+    struct insn_check_cast       : detail::insn_base<1, dex_type_hdl  > { using insn_base::insn_base; };
+    struct insn_instance_of      : detail::insn_base<2, dex_type_hdl  > { using insn_base::insn_base; };
+    struct insn_array_length     : detail::insn_base<2, no_const_val  > { using insn_base::insn_base; };
+    struct insn_new_instance     : detail::insn_base<1, dex_type_hdl  > { using insn_base::insn_base; };
+    struct insn_new_array        : detail::insn_base<2, dex_type_hdl  > { using insn_base::insn_base; };
+    struct insn_filled_new_array : detail::insn_base<5, dex_type_hdl  > { using insn_base::insn_base; };
+    struct insn_fill_array_data  : detail::insn_base<1, array_payload > { using insn_base::insn_base; };
+    struct insn_throw            : detail::insn_base<1, no_const_val  > { using insn_base::insn_base; };
+    struct insn_goto             : detail::insn_base<0, int32_t       > { using insn_base::insn_base; };
+    struct insn_switch           : detail::insn_base<1, no_const_val  > { using insn_base::insn_base; };
+    struct insn_cmp              : detail::insn_base<3, no_const_val  > { using insn_base::insn_base; };
+    struct insn_if               : detail::insn_base<2, no_const_val  > { using insn_base::insn_base; };
+    struct insn_if_z             : detail::insn_base<1, no_const_val  > { using insn_base::insn_base; };
+    struct insn_aget             : detail::insn_base<3, no_const_val  > { using insn_base::insn_base; };
+    struct insn_aput             : detail::insn_base<3, no_const_val  > { using insn_base::insn_base; };
+    struct insn_iget             : detail::insn_base<2, dex_field_hdl > { using insn_base::insn_base; };
+    struct insn_iget_quick       : detail::insn_base<2, uint16_t      > { using insn_base::insn_base; };
+    struct insn_iput             : detail::insn_base<2, dex_field_hdl > { using insn_base::insn_base; };
+    struct insn_iput_quick       : detail::insn_base<2, uint16_t      > { using insn_base::insn_base; };
+    struct insn_sget             : detail::insn_base<1, dex_field_hdl > { using insn_base::insn_base; };
+    struct insn_sput             : detail::insn_base<1, dex_field_hdl > { using insn_base::insn_base; };
+    struct insn_invoke           : detail::insn_base<5, dex_method_hdl> { using insn_base::insn_base; };
+    struct insn_invoke_quick     : detail::insn_base<5, uint16_t      > { using insn_base::insn_base; };
+    struct insn_unary_arith_op   : detail::insn_base<2, no_const_val  > { using insn_base::insn_base; };
+    struct insn_binary_arith_op  : detail::insn_base<3, no_const_val  > { using insn_base::insn_base; };
+    struct insn_aug_assign_op    : detail::insn_base<2, no_const_val  > { using insn_base::insn_base; };
+    struct insn_const_arith_op   : detail::insn_base<2, int16_t       > { using insn_base::insn_base; };
 
-    struct insn_entry : insn_base<5, no_const_val> { using insn_base::insn_base; };
-    struct insn_exit  : insn_base<1, no_const_val> { using insn_base::insn_base; };
+    struct insn_entry : detail::insn_base<5, no_const_val> { using insn_base::insn_base; };
+    struct insn_exit  : detail::insn_base<1, no_const_val> { using insn_base::insn_base; };
     // clang-format on
 
     inline std::ostream& operator<<(std::ostream& os, const insn_entry& x)
